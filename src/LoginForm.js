c0102bbecf5ba3bb0c5a732e788fd1b81b41b5bd
Vlad <vldsbk11@gmail.com>
2020-01-08 19:49:00 +0200

    made validation

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,19 +1,51 @@
 import React from "react";
 import { Formik } from "formik";
+import * as EmailValidator from "email-validator";
 
-const LoginForm = () => (
+const ValidatedLoginForm = () => (
   <Formik
     initialValues={{ email: "", password: "" }}
-    onSubmit={(values) => {
-      console.log("Submitting");
-      console.log(values);
+
+    onSubmit={(values, { setSubmitting }) => {
+      setTimeout(() => {
+        console.log("Logging in", values);
+        setSubmitting(false);
+      }, 500);
     }}
+    
+    validate={values => {
+      let errors = {};
+      if (!values.email) {
+        errors.email = "Required";
+      } else if (!EmailValidator.validate(values.email)) {
+        errors.email = "Invalid email address";
+      }
+
+      const passwordRegex = /(?=.*[0-9])/;
+      if (!values.password) {
+        errors.password = "Required";
+      } else if (values.password.length < 8) {
+        errors.password = "Password must be 8 characters long.";
+      } else if (!passwordRegex.test(values.password)) {
+        errors.password = "Invalid password. Must contain one number";
+      }
+
+      return errors;
+    }}
+    
+
   >
     {props => {
-      const { values, handleChange, handleSubmit } = props;
+      const {
+        values,
+        touched,
+        errors,
+        isSubmitting,
+        handleChange,
+        handleSubmit
+      } = props;
       return (
         <form onSubmit={handleSubmit}>
-          <h1>Login Form</h1>
           <label htmlFor="email">Email</label>
           <input
             name="email"
@@ -21,8 +53,11 @@ const LoginForm = () => (
             placeholder="Enter your email"
             value={values.email}
             onChange={handleChange}
+            className={errors.email && touched.email && "error"}
           />
-
+          {errors.email && touched.email && (
+            <div className="input-feedback">{errors.email}</div>
+          )}
           <label htmlFor="email">Password</label>
           <input
             name="password"
@@ -30,13 +65,18 @@ const LoginForm = () => (
             placeholder="Enter your password"
             value={values.password}
             onChange={handleChange}
+            className={errors.password && touched.password && "error"}
           />
-
-          <button type="submit">Login</button>
+          {errors.password && touched.password && (
+            <div className="input-feedback">{errors.password}</div>
+          )}
+          <button type="submit" disabled={isSubmitting}>
+            Login
+          </button>
         </form>
       );
     }}
   </Formik>
 );
 
-export default LoginForm;
+export default ValidatedLoginForm;
