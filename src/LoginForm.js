2829eaa1a83b577349540ecb828a9695891c7c1f
Vlad <vldsbk11@gmail.com>
2020-01-10 16:28:28 +0200

    made routes

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,82 +1,84 @@
 import React from "react";
-import { Formik } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as EmailValidator from "email-validator";
 
-const ValidatedLoginForm = () => (
-  <Formik
-    initialValues={{ email: "", password: "" }}
+export default class Login extends React.Component {
+  render() {
+    const { history } = this.props;
+    return (
+      <Formik
+        initialValues={{
+          email: "",
+          password: ""
+        }}
+        validate={values => {
+          let errors = {};
+          if (!values.email) {
+            errors.email = "Required";
+          } else if (!EmailValidator.validate(values.email)) {
+            errors.email = "Invalid email address";
+          }
 
-    onSubmit={(values, { setSubmitting }) => {
-      setTimeout(() => {
-        console.log("Logging in", values);
-        setSubmitting(false);
-      }, 500);
-    }}
-    
-    validate={values => {
-      let errors = {};
-      if (!values.email) {
-        errors.email = "Required";
-      } else if (!EmailValidator.validate(values.email)) {
-        errors.email = "Invalid email address";
-      }
+          const passwordRegex = /(?=.*[0-9])/;
+          if (!values.password) {
+            errors.password = "Required";
+          } else if (values.password.length < 8) {
+            errors.password = "Password must be 8 characters long.";
+          } else if (!passwordRegex.test(values.password)) {
+            errors.password = "Invalid password. Must contain one number";
+          }
 
-      const passwordRegex = /(?=.*[0-9])/;
-      if (!values.password) {
-        errors.password = "Required";
-      } else if (values.password.length < 8) {
-        errors.password = "Password must be 8 characters long.";
-      } else if (!passwordRegex.test(values.password)) {
-        errors.password = "Invalid password. Must contain one number";
-      }
-
-      return errors;
-    }}
-    
-
-  >
-    {props => {
-      const {
-        values,
-        touched,
-        errors,
-        isSubmitting,
-        handleChange,
-        handleSubmit
-      } = props;
-      return (
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">Email</label>
-          <input
-            name="email"
-            type="text"
-            placeholder="Enter your email"
-            value={values.email}
-            onChange={handleChange}
-            className={errors.email && touched.email && "error"}
-          />
-          {errors.email && touched.email && (
-            <div className="input-feedback">{errors.email}</div>
-          )}
-          <label htmlFor="email">Password</label>
-          <input
-            name="password"
-            type="password"
-            placeholder="Enter your password"
-            value={values.password}
-            onChange={handleChange}
-            className={errors.password && touched.password && "error"}
-          />
-          {errors.password && touched.password && (
-            <div className="input-feedback">{errors.password}</div>
-          )}
-          <button type="submit" disabled={isSubmitting}>
-            Login
-          </button>
-        </form>
-      );
-    }}
-  </Formik>
-);
-
-export default ValidatedLoginForm;
+          return errors;
+        }}
+        onSubmit={values => {
+          console.log("Logging in", values);
+          history.push("/home");
+        }}
+        render={({ errors, touched }) => (
+          <Form>
+            <div>
+              <h2>Login Page</h2>
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <Field
+                name="email"
+                type="text"
+                className={
+                  "form-control" +
+                  (errors.email && touched.email ? "error" : "")
+                }
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="input-feedback"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <Field
+                name="password"
+                type="password"
+                className={
+                  "form-control" +
+                  (errors.password && touched.password ? "error" : "")
+                }
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="input-feedback"
+              />
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
+          </Form>
+        )}
+      />
+    );
+  }
+}
